25ed2b22e3e2549d4ace5aa7355fb97ef8826587
ishuuu609 <ishandeep6081@gmail.com>
2025-10-23 01:27:32 +0530

    Added lab tasks

---
+++ c_programming_lab/LAB_1/introduction.c
@@ -0,0 +1,9 @@
+include<stdio.h>
+int main() {
+    printf("Name of student- Ishandeep Gupta\n");
+    printf("Branch-BTech Engineering \n");
+    printf("Roll no- MEBM25002 \n");
+    printf("University- Tezpur University");
+    return 0;
+    
+}
